ac88bd7889d04d23c7678dedef4b7a39373bbfd6
brian <yangbri1@msu.edu>
2024-11-20 10:40:35 -0500

    install 'react-router-dom' from respective npm library, import into 'main.jsx' (never alter normally), wrap <App /> component w/ <BrowserRouter>

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// "to give illusion of pages"
+import { BrowserRouter } from 'react-router-dom';
+
+// 1. npm i react-router-dom
+// 2. import "BrowserRoute" to main.jsx
+// 3. wrap <App /> component inside main.jsx (or index.js dep on framework) w/ <BrowserRouter> to allow children access to it
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
