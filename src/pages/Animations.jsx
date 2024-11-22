b26bb42599068d725fe89162369f0e3261a00559
brian <yangbri1@msu.edu>
2024-11-22 02:31:13 -0500

    more compact version (same as in 'Literary_Works.jsx' page) works for the time being, onto dynamic pages per animations by their unique '_id'

--- src/pages/Animations.jsx
+++ src/pages/Animations.jsx
@@ -16,52 +16,41 @@ export default function Animations(){
     //     <h1>Welcome {name}!!</h1>
     // )
 
-    // async function to retrieve data from MongoDB db
-    async function getData(){
-        try {
-            let res = await fetch(`http://localhost:3000/animations/`);
-            let data = await res.json();  // convert data into readable JSON format
-        
-            setAnimations(data);
-        } catch (err) {
-            console.error(err);
-        
-        }
-    }  
-
     // useEffect setup w/ arrow fn
     useEffect(() => {
-    // invoke getData() fn to connect to custom server-side db & retrieve collections from db
-    getData();
-    }, []);   // dependencies list/array of [] means only run on initial render 
+        // async function to retrieve data from MongoDB db
+        async function getData(){
+            try {
+                let res = await fetch(`http://localhost:3000/animations/`);
+                let data = await res.json();    // this step automatically done if using axios
+                
+                setAnimations(data);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        // invoke getData() fn to connect to custom server-side db & retrieve collections from db
+        getData();
+    }, []); // dependencies array set to empty -- only run once on initial render
 
-    // display when data do NOT exist
-    const loading = () => {
-    return(<h1>Loading (insert spinner)</h1>)
-    }
-    // loaded function when data is successfully fetched
-    const loaded = () => {
     return(
-        // functional components outside of <Routes> components DN change per page, each Route/page may change
-    <>
-        <h1>List of Animations</h1>
-        <div className="animations">
-            {/* create a copy of animations using array .map() method where ... */}
-            {animations.map((piece) => {
-                // destructure out properties of Animations obj dataset from backend
-                const { name, year} = piece;
-                return(
-                    // populate page with fetched animations datasets & dynamically <Link> up each one to own page
-                    <Link key={name} to={`/animations/animation/${piece._id}`}>
-                        <h3>{name} [{year}]</h3>
-                    </Link>
-                );
-            })}
-        </div>
-    </>
-    );
-    }
-    // if animations exist & has a year, invoke loaded() fn otw call forth loading()
-    return animations && animations.year ? loaded() : loading();
+        <>
+            <h1>List of Animations</h1>
+            <div className="animations">
+                {/* create a copy of animations using array .map() method where ... */}
+                {animations ? animations.map((piece) => {
+                    // destructure out "props" in Animations obj dataset from backend 
+                    const { name, year } = piece;
+                    // create some <link> components to listed works dynamically
+                    return(
+                        // populate page with fetched animations datasets & dynamically <Link> up each one to own page
+                        <Link key={piece._id} to={`/animations/animation/${piece._id}`}>
+                            <h3>{name} [{year}]</h3>
+                        </Link>
+                    );
+                }) : <h1>Loading ... </h1>}
+            </div>
+        </>
     
+    );
 }
